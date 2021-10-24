0ec88832b967891fe0259d5ca5f6f1621304b831
Saimon <saimon.ventura@freshbooks.com>
2021-10-24 10:26:43 -0300

    finish flask tutorial

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -9,6 +15,7 @@ bp = Blueprint("blog", __name__)
 
 @bp.route("/")
 def index():
+    """Show all the posts, most recent first."""
     db = get_db()
     posts = db.execute(
         "SELECT p.id, title, body, created, author_id, username"
@@ -18,32 +25,16 @@ def index():
     return render_template("blog/index.html", posts=posts)
 
 
-@bp.route("/create", methods=("GET", "POST"))
-@login_required
-def create():
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
-
-        if not title:
-            error = "Title is required."
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (title, body, author_id)" " VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
-            )
-            db.commit()
-            return redirect(url_for("blog.index"))
-
-    return render_template("blog/create.html")
-
-
 def get_post(id, check_author=True):
+    """Get a post and its author by id.
+    Checks that the id exists and optionally that the current user is
+    the author.
+    :param id: id of post to get
+    :param check_author: require the current user to be the author
+    :return: the post with author information
+    :raise 404: if a post with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
     post = (
         get_db()
         .execute(
@@ -64,9 +55,36 @@ def get_post(id, check_author=True):
     return post
 
 
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    """Create a new post for the current user."""
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.html")
+
+
 @bp.route("/<int:id>/update", methods=("GET", "POST"))
 @login_required
 def update(id):
+    """Update a post if the current user is the author."""
     post = get_post(id)
 
     if request.method == "POST":
@@ -82,7 +100,7 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE post SET title = ?, body = ?" " WHERE id = ?", (title, body, id)
+                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
             )
             db.commit()
             return redirect(url_for("blog.index"))
@@ -93,6 +111,10 @@ def update(id):
 @bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
 def delete(id):
+    """Delete a post.
+    Ensures that the post exists and that the logged in user is the
+    author of the post.
+    """
     get_post(id)
     db = get_db()
     db.execute("DELETE FROM post WHERE id = ?", (id,))
